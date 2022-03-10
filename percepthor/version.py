2dc5672ac63837c8d510e3a8fb0a30259a7d40dd
ermiry <erick.salas@ermiry.com>
2022-03-10 15:21:21 -0600

    Version 0.1

--- percepthor/version.py
+++ percepthor/version.py
@@ -1,45 +1,37 @@
 from cerver.utils.log import LOG_TYPE_NONE, cerver_log_both
 
-from .lib import lib
+PERCEPTHOR_VERSION = "0.1"
+PERCEPTHOR_VERSION_NAME = "Version 0.1"
+PERCEPTHOR_VERSION_DATE = "10/03/2022"
+PERCEPTHOR_VERSION_TIME = "15:20 CST"
+PERCEPTHOR_VERSION_AUTHOR = "Erick Salas"
 
-PYPERCEPTHOR_VERSION = "0.1".encode ('utf-8')
-PYPERCEPTHOR_VERSION_NAME = "Version 0.1".encode ('utf-8')
-PYPERCEPTHOR_VERSION_DATE = "12/07/2021".encode ('utf-8')
-PYPERCEPTHOR_VERSION_TIME = "09:54 CST".encode ('utf-8')
-PYPERCEPTHOR_VERSION_AUTHOR = "Erick Salas".encode ('utf-8')
-
-percepthor_libauth_version_print_full = lib.percepthor_libauth_version_print_full
-percepthor_libauth_version_print_version_id = lib.percepthor_libauth_version_print_version_id
-percepthor_libauth_version_print_version_name = lib.percepthor_libauth_version_print_version_name
+version = {
+	"id": PERCEPTHOR_VERSION,
+	"name": PERCEPTHOR_VERSION_NAME,
+	"date": PERCEPTHOR_VERSION_DATE,
+	"time": PERCEPTHOR_VERSION_TIME,
+	"author": PERCEPTHOR_VERSION_AUTHOR
+}
 
 def pypercepthor_version_print_full ():
-	cerver_log_both (
-		LOG_TYPE_NONE, LOG_TYPE_NONE,
-		"\nPyPercepthor Version: %s".encode ('utf-8'), PYPERCEPTHOR_VERSION_NAME
-	)
+	output = "\nPyPercepthor Version: {name}\n" \
+		"Release Date: {date} - {time}\n" \
+		"Author: {author}\n".format (**version)
 
 	cerver_log_both (
 		LOG_TYPE_NONE, LOG_TYPE_NONE,
-		"Release Date & time: %s - %s".encode ('utf-8'),
-		PYPERCEPTHOR_VERSION_DATE, PYPERCEPTHOR_VERSION_TIME
-	)
-
-	cerver_log_both (
-		LOG_TYPE_NONE, LOG_TYPE_NONE,
-		"Author: %s\n".encode ('utf-8'),
-		PYPERCEPTHOR_VERSION_AUTHOR
+		output.encode ("utf-8")
 	)
 
 def pypercepthor_version_print_version_id ():
 	cerver_log_both (
 		LOG_TYPE_NONE, LOG_TYPE_NONE,
-		"\nPyPercepthor Version ID: %s\n".encode ('utf-8'),
-		PYPERCEPTHOR_VERSION
+		f"\nPyPercepthor Version ID: {version.id}\n".encode ("utf-8")
 	)
 
 def pypercepthor_version_print_version_name ():
 	cerver_log_both (
 		LOG_TYPE_NONE, LOG_TYPE_NONE,
-		"\nPyPercepthor Version: %s\n".encode ('utf-8'),
-		PYPERCEPTHOR_VERSION_NAME
+		f"\nPyPercepthor Version: {version.name}\n".encode ("utf-8")
 	)
