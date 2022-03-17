da514d1063064e56ab0999c028f1bab82d27391a
ermiry <erick.salas@ermiry.com>
2022-03-17 00:02:06 -0600

    Added ticket type & status to string methods

--- percepthor/models/ticket.py
+++ percepthor/models/ticket.py
@@ -5,6 +5,20 @@ TICKET_TYPE_NONE = 0
 TICKET_TYPE_REPORT = 1
 TICKET_TYPE_DISCOVER = 2
 
+def ticket_type_to_string (t_type: int):
+	result = "Undefined"
+
+	if (t_type == TICKET_TYPE_NONE):
+		pass
+
+	elif (t_type == TICKET_TYPE_REPORT):
+		result = "Report"
+
+	elif (t_type == TICKET_TYPE_DISCOVER):
+		result = "Discover"
+
+	return result
+
 TICKET_STATUS_NONE = 0
 TICKET_STATUS_CREATED = 1
 TICKET_STATUS_WAITING = 2
@@ -13,6 +27,35 @@ TICKET_STATUS_GOOD = 4
 TICKET_STATUS_BAD = 5
 TICKET_STATUS_PASSED = 6
 TICKET_STATUS_ERROR = 7
+
+def ticket_status_to_string (status: int):
+	result = "Undefined"
+
+	if (status == TICKET_STATUS_NONE):
+		pass
+
+	elif (status == TICKET_STATUS_CREATED):
+		result = "Created"
+
+	elif (status == TICKET_STATUS_WAITING):
+		result = "Waiting"
+
+	elif (status == TICKET_STATUS_IN_REVIEW):
+		result = "In Review"
+
+	elif (status == TICKET_STATUS_GOOD):
+		result = "Good"
+
+	elif (status == TICKET_STATUS_BAD):
+		result = "Bad"
+
+	elif (status == TICKET_STATUS_PASSED):
+		result = "Passed"
+
+	elif (status == TICKET_STATUS_ERROR):
+		result = "Error"
+
+	return result
 
 class Ticket (Schema):
 	schema_name = "tickets"
