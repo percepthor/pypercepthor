1b0798451a30741e5013d8a68bfdde8a84f5ea24
ermiry <erick.salas@ermiry.com>
2022-03-18 23:49:44 -0600

    Updated token model to handle user values

--- percepthor/models/token.py
+++ percepthor/models/token.py
@@ -1,6 +1,32 @@
 import datetime
 from pymongoose.mongo_types import Types, Schema
 
+TOKEN_TYPE_NONE = 0
+TOKEN_TYPE_NORMAL = 1
+TOKEN_TYPE_TEMPORARY = 2
+TOKEN_TYPE_QUANTITY = 3
+TOKEN_TYPE_USER = 4
+
+def token_type_to_string (t_type: int):
+	result = "Undefined"
+
+	if (t_type == TOKEN_TYPE_NONE):
+		pass
+
+	elif (t_type == TOKEN_TYPE_NORMAL):
+		result = "Normal"
+
+	elif (t_type == TOKEN_TYPE_TEMPORARY):
+		result = "Temporary"
+
+	elif (t_type == TOKEN_TYPE_QUANTITY):
+		result = "Quantity"
+
+	elif (t_type == TOKEN_TYPE_USER):
+		result = "User"
+
+	return result
+
 TOKEN_STATUS_NONE = 0
 TOKEN_STATUS_AVAILABLE = 1
 TOKEN_STATUS_EXPIRED = 2
@@ -27,33 +53,16 @@ def token_status_to_string (status: int):
 
 	return result
 
-TOKEN_TYPE_NONE = 0
-TOKEN_TYPE_NORMAL = 1
-TOKEN_TYPE_TEMPORARY = 2
-TOKEN_TYPE_QUANTITY = 3
-
-def token_type_to_string (status: int):
-	result = "Undefined"
-
-	if (status == TOKEN_TYPE_NONE):
-		pass
-
-	elif (status == TOKEN_TYPE_NORMAL):
-		result = "Normal"
-
-	elif (status == TOKEN_TYPE_TEMPORARY):
-		result = "Temporary"
-
-	elif (status == TOKEN_TYPE_QUANTITY):
-		result = "Quantity"
-
-	return result
-
 class Token (Schema):
 	schema_name = "tokens"
 
 	def __init__ (self, **kwargs):
 		self.schema = {
+			"organization": {
+				"type": Types.ObjectId,
+				"ref": "organizations",
+				"default": None
+			},
 			"t_type": {
 				"type": Types.Number,
 				"default": TOKEN_TYPE_NONE
@@ -62,12 +71,27 @@ class Token (Schema):
 				"type": Types.Number,
 				"default": TOKEN_STATUS_NONE
 			},
+
+			# owner
 			"user": {
 				"type": Types.ObjectId,
 				"ref": "users",
 				"required": True
 			},
-			"name": {
+
+			# user values
+			"role": {
+				"type": Types.ObjectId,
+				"ref": "roles",
+				"default": None
+			},
+			"username": {
+				"type": Types.String,
+				"default": None
+			},
+
+			# api key
+			"token_name": {
 				"type": Types.String,
 				"required": True
 			},
@@ -75,20 +99,32 @@ class Token (Schema):
 				"type": Types.String,
 				"default": None
 			},
+
+			# actual token values
+			"iat": {
+				"type": Types.Number,
+				"default": 0
+			},
 			"value": {
 				"type": Types.String,
 				"required": True
 			},
-			"data": {
-				"type": Types.ObjectId,
-				"ref": "tokens.data",
-				"required": True
+
+			# configuration
+			"expiration": {
+				"type": Types.Date,
+				"default": None
+			},
+			"quantity": {
+				"type": Types.Number,
+				"default": 0
 			},
 			"permissions": {
 				"type": Types.ObjectId,
 				"ref": "permissions",
 				"default": None
 			},
+
 			"created": {
 				"type": Types.Date,
 				"default": datetime.datetime.utcnow ()
@@ -104,16 +140,6 @@ class Token (Schema):
 			"revoked": {
 				"type": Types.Date,
 				"default": None
-			},
-			
-			"service": {
-				"type": Types.ObjectId,
-				"ref": "services",
-				"default": None
-			},
-			"mask": {
-				"type": Types.Number,
-				"default": 0
 			}
 		}
 
