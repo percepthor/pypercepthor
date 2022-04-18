ca11cfefac00b33b37fa3779e725b9903211d348
ermiry <erick.salas@ermiry.com>
2022-04-18 18:23:39 -0500

    Added custom model to keep track of machine updates

---
+++ percepthor/models/machine_update.py
@@ -0,0 +1,35 @@
+import datetime
+from pymongoose.mongo_types import Types, Schema
+
+from models.machine_status import MACHINE_STATUS_NONE
+
+class MachineUpdate (Schema):
+	schema_name = "machines.updates"
+
+	def __init__ (self, **kwargs):
+		self.schema = {
+			"machine": {
+				"type": Types.ObjectId,
+				"ref": "machines",
+				"default": None
+			},
+
+			"prev_status": {
+				"type": Types.Number,
+				"default": MACHINE_STATUS_NONE
+			},
+			"current_status": {
+				"type": Types.Number,
+				"default": MACHINE_STATUS_NONE
+			},
+
+			"date": {
+				"type": Types.Date,
+				"default": datetime.datetime.utcnow ()
+			}
+		}
+
+		super ().__init__ (self.schema_name, self.schema, kwargs)
+
+	def __str__ (self):
+		return f"MachineUpdate: {self.id}"
