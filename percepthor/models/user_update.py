8e64f4920b995feb60a73a6409066f3461dda3d7
ermiry <erick.salas@ermiry.com>
2022-04-19 10:44:26 -0500

    Added custom model to keep track of users updates

---
+++ percepthor/models/user_update.py
@@ -0,0 +1,35 @@
+import datetime
+from pymongoose.mongo_types import Types, Schema
+
+from models.user_status import USER_STATUS_NONE
+
+class UserUpdate (Schema):
+	schema_name = "users.updates"
+
+	def __init__ (self, **kwargs):
+		self.schema = {
+			"user": {
+				"type": Types.ObjectId,
+				"ref": "users",
+				"default": None
+			},
+
+			"prev_status": {
+				"type": Types.Number,
+				"default": USER_STATUS_NONE
+			},
+			"current_status": {
+				"type": Types.Number,
+				"default": USER_STATUS_NONE
+			},
+
+			"date": {
+				"type": Types.Date,
+				"default": datetime.datetime.utcnow ()
+			}
+		}
+
+		super ().__init__ (self.schema_name, self.schema, kwargs)
+
+	def __str__ (self):
+		return f"UserUpdate: {self.id}"
