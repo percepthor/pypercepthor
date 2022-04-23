55e8fbef0cefaad377a422fe5d945d3a1dd9876b
ermiry <erick.salas@ermiry.com>
2022-04-22 20:14:18 -0500

    Added base submodule schema model implementation

---
+++ percepthor/models/submodule_schema.py
@@ -0,0 +1,33 @@
+import datetime
+from pymongoose.mongo_types import Types, Schema
+
+class SubmoduleSchema (Schema):
+	schema_name = "submodules.schema"
+
+	def __init__ (self, **kwargs):
+		self.schema = {
+			"submodule": {
+				"type": Types.ObjectId,
+				"ref": "submodules",
+				"required": True
+			},
+			"configuration": {
+				"type": Types.ObjectId,
+				"ref": "submodules.configuration",
+				"required": True
+			},
+
+			"data": {
+				# we expect dynamic data
+			},
+
+			"date": {
+				"type": Types.Date,
+				"default": datetime.datetime.utcnow ()
+			}
+		}
+
+		super ().__init__ (self.schema_name, self.schema, kwargs)
+
+	def __str__ (self):
+		return f"SubmoduleSchema: {self.id}"
