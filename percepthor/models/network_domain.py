190d22df92f5c2937f1ffc8c6a03e0585132da79
ermiry <erick.salas@ermiry.com>
2022-04-25 12:08:09 -0500

    Added dedicated network domain model definition

---
+++ percepthor/models/network_domain.py
@@ -0,0 +1,32 @@
+import datetime
+from pymongoose.mongo_types import Types, Schema
+
+class NetworkDomain (Schema):
+	schema_name = "networks.domains"
+
+	def __init__ (self, **kwargs):
+		self.schema = {
+			"name": {
+				"type": Types.String,
+				"required": True
+			},
+			"description": {
+				"type": Types.String,
+				"required": True
+			},
+
+			"address": {
+				"type": Types.String,
+				"required": True
+			},
+
+			"date": {
+				"type": Types.Date,
+				"default": datetime.datetime.utcnow ()
+			}
+		}
+
+		super ().__init__ (self.schema_name, self.schema, kwargs)
+
+	def __str__ (self):
+		return f"NetworkDomain: {self.id}"
