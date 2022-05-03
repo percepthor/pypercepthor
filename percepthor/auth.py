184c2654bbee7bd68b9e9ad74ea6d5ebbc7dd386
ermiry <erick.salas@ermiry.com>
2022-05-03 07:55:38 -0500

    Added latest percepthor auth token related methods

--- percepthor/auth.py
+++ percepthor/auth.py
@@ -1,4 +1,4 @@
-from ctypes import c_void_p, c_char_p, c_int, c_uint, c_bool
+from ctypes import c_void_p, c_char_p, c_int, c_uint, c_int64, c_bool
 
 from .lib import lib
 
@@ -58,9 +58,44 @@ percepthor_auth_permissions_iter_get_next = lib.percepthor_auth_permissions_iter
 percepthor_auth_permissions_iter_get_next.argtypes = [c_void_p]
 percepthor_auth_permissions_iter_get_next.restype = c_void_p
 
+percepthor_auth_get_token_id = lib.percepthor_auth_get_token_id
+percepthor_auth_get_token_id.argtypes = [c_void_p]
+percepthor_auth_get_token_id.restype = c_char_p
+
+percepthor_auth_get_token_type = lib.percepthor_auth_get_token_type
+percepthor_auth_get_token_type.argtypes = [c_void_p]
+percepthor_auth_get_token_type.restype = c_int
+
+percepthor_auth_get_token_organization = lib.percepthor_auth_get_token_organization
+percepthor_auth_get_token_organization.argtypes = [c_void_p]
+percepthor_auth_get_token_organization.restype = c_char_p
+
+percepthor_auth_get_token_permissions = lib.percepthor_auth_get_token_permissions
+percepthor_auth_get_token_permissions.argtypes = [c_void_p]
+percepthor_auth_get_token_permissions.restype = c_char_p
+
+percepthor_auth_get_token_role = lib.percepthor_auth_get_token_role
+percepthor_auth_get_token_role.argtypes = [c_void_p]
+percepthor_auth_get_token_role.restype = c_char_p
+
+percepthor_auth_get_token_user = lib.percepthor_auth_get_token_user
+percepthor_auth_get_token_user.argtypes = [c_void_p]
+percepthor_auth_get_token_user.restype = c_char_p
+
+percepthor_auth_get_token_username = lib.percepthor_auth_get_token_username
+percepthor_auth_get_token_username.argtypes = [c_void_p]
+percepthor_auth_get_token_username.restype = c_char_p
+
+percepthor_auth_get_mask = lib.percepthor_auth_get_mask
+percepthor_auth_get_mask.argtypes = [c_void_p]
+percepthor_auth_get_mask.restype = c_int64
+
 percepthor_auth_create = lib.percepthor_auth_create
 percepthor_auth_create.argtypes = [c_int]
 percepthor_auth_create.restype = c_void_p
+
+percepthor_auth_print_token = lib.percepthor_auth_print_token
+percepthor_auth_print_token.argtypes = [c_void_p]
 
 percepthor_single_authentication = lib.percepthor_single_authentication
 percepthor_single_authentication.argtypes = [c_void_p, c_void_p, c_int, c_char_p, c_char_p]
