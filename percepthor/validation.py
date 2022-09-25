9858b97a2b9d4f8a0de206fe7fb19d1181e38f8e
ermiry <erick.salas@ermiry.com>
2022-09-24 21:44:25 -0500

    Added dedicated validation method to handle body input

--- percepthor/validation.py
+++ percepthor/validation.py
@@ -1,8 +1,12 @@
-from ctypes import c_void_p
+from ctypes import POINTER, c_void_p
 import distutils.util
+import json
 from typing import Any, Callable
 
+from cerver.types import String
+
 from cerver.http import http_query_pairs_get_value
+from cerver.http import http_request_get_body
 
 def percepthor_query_value_from_params (
 	query: dict, params: c_void_p, name: str, errors: dict
@@ -163,3 +167,20 @@ def percepthor_query_bool_optional_value_from_params (
 			)
 		except ValueError:
 			errors[name] = f"Field {name} is invalid."
+
+def percepthor_handle_body_input (
+	request: c_void_p, handle_body_input: Callable [[dict, dict], dict], errors: dict
+) -> dict:
+	values = None
+
+	body_json: POINTER (String) = http_request_get_body (request)
+
+	if (body_json is not None):
+		loaded_json: dict = json.loads (body_json.contents.str)
+
+		handle_body_input (loaded_json, errors)
+
+	else:
+		errors["body"] = "Request body input is required!"
+
+	return values
